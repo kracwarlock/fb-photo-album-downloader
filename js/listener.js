2938992d9159aaff007317e808469221e99805f4
kracwarlock <shikhar38@gmail.com>
2013-01-26 04:09:39 +0530

    Background Page & Experimental Scripts

---
+++ js/listener.js
@@ -0,0 +1,18 @@
+chrome.extension.onRequest.addListener(
+	function(request, sender, sendResponse)
+	{
+		chrome.tabs.create({"url":"chrome://downloads"});
+		for(i=0;i<request.arr.length;i++)
+		{
+			if(request.arr[i]!=null)
+			{
+				if(request.arr[i].indexOf('spacer') == -1)
+				{
+					console.log(request.arr[i]);
+					chrome.experimental.downloads.download({"url":request.arr[i]});
+					console.log(chrome.extension.lastError);
+				}
+			}
+		}
+	}
+);
